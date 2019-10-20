e3892e25bc76306973624977140bad2221ec47eb
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-20 03:53:42 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
  index = 0
  while index < directors_database[0][:movies].length do
-   puts directors_database[0][:movies][index]
+   puts directors_database[0][:movies][index][:title]
    index +=1
  end
 end
