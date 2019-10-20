e33b9706838b3528a61a24ecde48852ea840128d
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-20 03:52:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,8 +9,8 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
- row_index = 0
- while row_index < directors_database[Stephen Spielbirg].length do
-   puts directors_database[Stephen Spielbirg]
+ index = 0
+ while index < directors_database[0][:movies].length do
+   puts directors_database[0][:movies][index]
  end
 end
