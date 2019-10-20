74a946d498b838c9d1fd6deec60d428ed43c64a3
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-20 03:51:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
+require 'pp'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  puts pp nds
 end
 
 def print_first_directors_movie_titles
+ row_index = 0
+ while row_index < directors_database[Stephen Spielbirg].length do
+   puts directors_database[Stephen Spielbirg]
+ end
 end
